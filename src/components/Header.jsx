b2c47c68830154c6c02b51c8e3a25cd33b7ad0c8
Mikhail Nepomnyashchiy <nem@kodix.ru>
2022-02-07 17:25:16 +0100

    clear controls

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,6 +6,7 @@ import { IoMoon, IoMoonOutline } from 'react-icons/io5';
 
 import { Container } from './Container';
 import { setTheme } from '../store/theme/theme-actions';
+import { clearControls } from '../store/controls/controls-actions';
 
 const HeaderEl = styled.header`
   box-shadow: var(--shadow);
@@ -41,6 +42,8 @@ export const Header = () => {
 
   const toggleTheme = () => dispatch(setTheme(theme === 'light' ? 'dark' : 'light'))
 
+  const cleanUp = () => dispatch(clearControls());
+
   useEffect(() => {
     document.body.setAttribute('data-theme', theme);
   }, [theme]);
@@ -49,7 +52,7 @@ export const Header = () => {
     <HeaderEl>
       <Container>
         <Wrapper>
-          <Title>Where is the world?</Title>
+          <Title onClick={cleanUp}>Where is the world?</Title>
           <ModeSwitcher onClick={toggleTheme}>
             {theme === 'light' ? (
               <IoMoonOutline size="14px" />
--- src/store/controls/controls-reducer.js
+++ src/store/controls/controls-reducer.js
@@ -1,4 +1,4 @@
-import { SET_REGION, SET_SEARCH } from './controls-actions';
+import { CLEAR_CONTROLS, SET_REGION, SET_SEARCH } from './controls-actions';
 
 const initialState = {
   search: '',
@@ -17,6 +17,8 @@ export const controlsReducer = (state = initialState, {type, payload}) => {
         ...state,
         region: payload,
       }
+    case CLEAR_CONTROLS:
+      return initialState;
     default:
       return state;
   }
